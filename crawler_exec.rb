b217bbf0c7d9165d9177ecb7635438c2ea844d7c
seiichi <seiichi19881101@gmail.com>
2017-10-21 02:32:29 +0900

    change db from mysql to cassandra

--- crawler_exec.rb
+++ crawler_exec.rb
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 require "twitter"
 require "mysql"
+require "cassandra"
 require "date"
 require "inifile"
 
@@ -31,43 +32,20 @@ class Fetching_tw
             result_tweets = @client.search(@query, result_type:"recent", exclude: "retweets", since_id:@since_id)
             result_tweets.take(50).each_with_index do |tw, i|
                 @data[i] = [tw.id, tw.user.screen_name, tw.full_text]
-                #  次はこのID以降のTweetが取得される
                 @since_id = tw.id
             end
-                # 検索ワードで Tweet を取得できなかった場合の例外処理
         rescue Twitter::Error::ClientError
             puts "**Error**"
             sleep(60)
             retry
         end
-#        p @data
+      #  p @data
     end
 
     def getData()
         return @data
     end
 
-end
-class Mymysql
-    def initialize(q)
-        #    tblname="tbl_"+DateTime.now.strftime("%Y_%m_%d_%H_%M_%S").to_s
-        @dbname="tw_data"
-        @tblname=q
-
-        @client= Mysql.connect("127.0.0.1", "root", "root")
-        @client.query("set character set utf8mb4")
-        @client.query("CREATE DATABASE IF NOT EXISTS #{@dbname}")
-        @client.query("USE #{@dbname}")
-        @client.query("create table if not EXISTS #{@tblname} (id char(18), name char(32), text varchar(255), date DATETIME(6))")
-    end
-    def set_to_mysql(data)
-      date = DateTime.now.strftime('%Y-%m-%d %I:%M:%S')
-      puts data
-      data.each do |elt|
-        elt[2] = elt[2].gsub(/['']/) {|ch| ch + ch }
-        @client.query("insert into #{@tblname}(id, name, text, date)VALUES('#{elt[0].to_s}', '#{elt[1].to_s}', '#{elt[2].to_s}', '#{date}')")
-      end
-    end
 end
 
 def _get(inifile, section, name)
@@ -77,7 +55,6 @@ def _get(inifile, section, name)
     return "error: could not read #{name} msg:"+e
   end
 end
-
 
 query = ""
 
@@ -90,6 +67,24 @@ if (query == "") then
   exit(1)
 end
 
+cluster = Cassandra.cluster
+
+keyspace_name = 'twitter'
+session  = cluster.connect(keyspace_name)
+
+table_definition = <<-TABLE_CQL
+  CREATE TABLE IF NOT EXISTS #{query} (
+    id VARCHAR,
+    name VARCHAR,
+    text TEXT,
+    date TIMESTAMP,
+    PRIMARY KEY (id)
+  )
+TABLE_CQL
+
+session.execute(table_definition)
+
+statement = session.prepare('INSERT INTO weed (id, name, text, date) VALUES (?, ?, ?, ?)')
 
 puts ("クエリ： "+query)
 
@@ -97,9 +92,11 @@ tw = Fetching_tw.new(query)
 while true
     puts ("---------------------get data from twitter-----------------------")
     tw.fetching
-    puts ("---------------------put data to mysql-----------------------")
-    sql = Mymysql.new(query)
-    sql.set_to_mysql(tw.getData())
+    puts ("---------------------put data to cassandra-----------------------")
+    tw.getData.each do |elt|
+      elt[2] = elt[2].gsub(/['']/) {|ch| ch + ch }
+      session.execute(statement, arguments: [elt[0].to_s, elt[1].to_s, elt[2].to_s, DateTime.now.to_time])
+    end
     puts ("---------------------sleep for 60s-----------------------")
     sleep(60)
 end
